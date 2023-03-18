50e80aeb3ebc038f054a386ac198d45cc3c519ff
Maiky-ZnfC <look2012a@gmail.com>
2023-03-18 10:35:26 -0500

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,19 @@
+$(document).ready(function(){
+	var height = $(window).height();
+	inicio();
+	function inicio() {
+		$("section#body").css({
+			"margin-top": height - 80 + "px"
+		})
+	}
+	$(document).scroll(function(){
+		var scrollTop = $(this).scrollTop();
+		var pixels=scrollTop/70;
+		if (scrollTop<height) {
+			$("section#contenedor_general").css({
+				"-webkit-filter": "blur("+pixels+"px)",
+				"background-position": "center -"+pixels*10+"px"
+			});
+		}
+	});
+});
